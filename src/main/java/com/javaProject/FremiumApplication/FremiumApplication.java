3817381441d81040cb3f07950ba5fc3277660cf5
Your Name <nehaprasad.kvs@gmail.com>
2025-07-06 13:36:29 +0530

    fremium blog project using jwt and mongodb

---
+++ src/main/java/com/javaProject/FremiumApplication/FremiumApplication.java
@@ -0,0 +1,13 @@
+package com.javaProject.FremiumApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FremiumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FremiumApplication.class, args);
+	}
+
+}
